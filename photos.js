c1b84d0139128aea96cd652b073c0f21e23a524c
zekiyildiz <153931207+zekiyildiz@users.noreply.github.com>
2025-04-30 00:14:03 +0300

    photos with jquery plugin done

---
+++ photos.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#imageGallery").lightSlider({
+        gallery: true,
+        item: 1,
+        loop: true,
+        slideMargin: 0,
+        thumbItem: 5
+    });
+});
